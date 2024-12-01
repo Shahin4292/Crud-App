78e7d93ab1aea91f1daa47393cabfc6dfc20437e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-01 12:11:47 +0600

    note screen added

---
+++ lib/views/note_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
