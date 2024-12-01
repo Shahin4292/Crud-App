6f631bdc2c5c3e248b5d656ad2f7d6cea559a6ce
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-01 13:02:57 +0600

    done

--- lib/widget/custom_single_note.dart
+++ lib/widget/custom_single_note.dart
@@ -3,11 +3,20 @@ import 'package:crud_getx/utils/modify_text.dart';
 import 'package:flutter/material.dart';
 
 class CustomSingleNote extends StatelessWidget {
-  const CustomSingleNote({super.key});
+  final String title;
+  final String description;
+  final String date;
+
+  const CustomSingleNote(
+      {super.key,
+      required this.title,
+      required this.description,
+      required this.date});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(bottom: 10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         color: Colors.white,
@@ -16,35 +25,39 @@ class CustomSingleNote extends StatelessWidget {
         ),
       ),
       child: ListTile(
-        horizontalTitleGap: 0,
-        leading: Container(
-          height: 13,
-          width: 13,
-          decoration: const BoxDecoration(
-              shape: BoxShape.circle, color: AppColors.backgroundColor),
-        ),
-        subtitle: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ModifyText(
-                text: "Hi There this is a test notes",
-                color: Colors.black,
-                size: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ModifyText(text: "12/01/24", color: Colors.black, size: 10),
-                Icon(Icons.delete),
-              ],
-            )
-          ],
-        ),
-        title: const ModifyText(
-          text: 'Software Developer',
-          color: Colors.black,
-          size: 16,
-        ),
-      ),
+          horizontalTitleGap: 0,
+          leading: Container(
+            height: 13,
+            width: 13,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle, color: AppColors.backgroundColor),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                description,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ModifyText(text: date, color: Colors.black, size: 10),
+                  InkWell(onTap: () {}, child: const Icon(Icons.delete)),
+                ],
+              )
+            ],
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(
+                fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
+          )),
     );
   }
 }
