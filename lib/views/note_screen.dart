2820d85139496e853cc7fc086a407d3d3da11192
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-01 13:00:33 +0600

    ui part almost complete

--- lib/views/note_screen.dart
+++ lib/views/note_screen.dart
@@ -1,3 +1,5 @@
+import 'package:crud_getx/utils/colors.dart';
+import 'package:crud_getx/utils/modify_text.dart';
 import 'package:flutter/material.dart';
 
 class NoteScreen extends StatelessWidget {
@@ -6,7 +8,97 @@ class NoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: AppColors.backgroundColor,
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Divider(),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.link,
+              size: 20,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.share,
+              size: 20,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+              size: 20,
+            ),
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 12, right: 12, bottom: 20),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor: AppColors.primaryColor,
+              fixedSize: Size(MediaQuery.sizeOf(context).width, 50)),
+          child: const ModifyText(
+            text: 'Save Changes',
+            color: Colors.white,
+            size: 18,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Column(
+          children: [
+            TextFormField(
+              maxLines: 2,
+              minLines: 1,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.black),
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: "Title",
+                // hintStyle: TextStyle(
+                //     fontWeight: FontWeight.w700,
+                //     fontSize: 30,
+                //     color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: TextFormField(
+                minLines: 30,
+                maxLines: 30,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.black),
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Description",
+                  // hintStyle: TextStyle(
+                  //     color: Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
